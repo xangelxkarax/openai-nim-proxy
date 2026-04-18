625b37906de90ca6f615c95daf1f9647a8a09dca
xangelxkarax <vorpalverav@gmail.com>
2026-04-18 23:35:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
